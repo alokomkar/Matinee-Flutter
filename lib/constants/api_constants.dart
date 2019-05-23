725bafc90a8153eda0b06f0287e200e50e662175
Alok <alok@wizely.in>
2019-05-23 10:58:08 +0530

    Cleanup in main screen

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,3 +1,3 @@
 const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
-const String TMDB_API_KEY = "YOUR_API_KEY";
+const String TMDB_API_KEY = "f4b2ebc8b5ef373a0c7e1d4d954fb181";
 const String TMDB_BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
